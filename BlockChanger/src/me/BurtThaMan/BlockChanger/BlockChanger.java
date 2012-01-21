27d8ae9cf73e6452d910af878a176cc690f02088
BurtThaMan <Matt@HomePC.(none)>
2012-01-21 07:40:20 -0600

    updated project name and started coding

--- BlockChanger/src/me/BurtThaMan/BlockChanger/BlockChanger.java
+++ BlockChanger/src/me/BurtThaMan/BlockChanger/BlockChanger.java
@@ -1,5 +1,21 @@
 package me.BurtThaMan.BlockChanger;
 
-public class BlockChanger {
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BlockChanger extends JavaPlugin implements Listener
+{
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
